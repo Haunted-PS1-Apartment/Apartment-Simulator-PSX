f9067784ad332509ed71c07e4cf43b0603881bc3
BluShine <BluShine@users.noreply.github.com>
2020-08-28 23:19:51 -0500

    Added interactive spooky eye.

---
+++ Assets/Scripts/EyeTrigger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeTrigger : MonoBehaviour
+{
+    [SerializeField] float time = .5f;
+    [SerializeField] Vector3 offset = Vector3.zero;
+    [SerializeField] AnimationCurve curve;
+    [SerializeField] GameObject disableObject;
+    [SerializeField] GameObject eyeObject;
+
+    float startTime = 0;
+    Vector3 originalPosition;
+
+    void OnEnable()
+    {
+        originalPosition = eyeObject.transform.localPosition;
+        startTime = time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(time == 0)
+        {
+            return;
+        }
+        time -= Time.deltaTime;
+        time = Math.Max(0, time);
+        eyeObject.transform.localPosition = originalPosition + offset * curve.Evaluate(1 - time / startTime);
+        if(time == 0)
+        {
+            disableObject.SetActive(false);
+        }
+    }
+}
