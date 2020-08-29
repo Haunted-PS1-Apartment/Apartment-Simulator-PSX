59b61b146d12f79b4fe760a2faf217df6f96e152
BluShine <BluShine@users.noreply.github.com>
2020-08-28 19:00:12 -0500

    Added room cameras.

---
+++ Assets/Scripts/SpinAnimator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinAnimator : MonoBehaviour
+{
+
+    [SerializeField] float speed = 1;
+    float increment = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        increment += Time.deltaTime * speed;
+        increment = increment % 1;
+        transform.rotation = Quaternion.Euler(0, increment * 360, 0);
+    }
+}
